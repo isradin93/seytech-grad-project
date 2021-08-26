670ad2627fab7529df6dc89fffc7cea6ec35cd4b
Isradin <isradin.nurbek@gmail.com>
2021-08-26 21:52:17 +0300

    Finish project

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,13 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', () => {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -29,11 +30,24 @@ gulp.task('styles', () => gulp.src("src/sass/**/*.+(scss|sass)")
     .pipe(cleanCSS({
         compatibility: 'ie8'
     }))
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream()));
 
 gulp.task('watch', () => {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', () => gulp.src("src/*.html")
+    .pipe(htmlmin({
+        collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("dist/")));
+
+gulp.task('fonts', () => gulp.src("src/fonts/**/*")
+    .pipe(gulp.dest("dist/fonts")));
+
+gulp.task('images', () => gulp.src("src/img/**/*")
+    .pipe(gulp.dest("dist/img")));
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'fonts', 'html', 'images'));
